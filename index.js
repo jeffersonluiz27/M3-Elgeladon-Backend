22fde6e353061e53a7cccf3d9254aafddd489a84
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-04-12 21:48:29 -0300

    acescentando variavel paletas

--- index.js
+++ index.js
@@ -2,6 +2,35 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
+app.use(express.json());
+
+const paletas = [
+  {
+    id: 1,
+    sabor: 'Açaí com Leite Condensado',
+    descricao:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: 'assets/images/acai-com-leite-condensado.png',
+    preco: 10.0,
+  },
+  {
+    id: 2,
+    sabor: 'Banana com Nutella',
+    descricao:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: 'assets/images/banana-com-nutella.png',
+    preco: 9.0,
+  },
+  {
+    id: 3,
+    sabor: 'Chocolate Belga',
+    descricao:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: 'assets/images/chocolate-belga.png',
+    preco: 7.0,
+  },
+];
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
