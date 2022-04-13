c668f0538d8a1125847d501b96d86ef497510fd7
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-04-12 22:07:20 -0300

    chamada cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const port = 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const paletas = [
   {
