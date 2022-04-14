9b030c4bda6ca886bbedd210d899e3d290d0d2ae
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-04-13 22:45:49 -0300

    Estrutura MVC

--- index.js
+++ index.js
@@ -1,51 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const route = require('./src/routes/paletas.route');
+
 const port = 3000;
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-const paletas = [
-  {
-    id: 1,
-    sabor: 'Açaí com Leite Condensado',
-    descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-    foto: 'assets/images/acai-com-leite-condensado.png',
-    preco: 10.0,
-  },
-  {
-    id: 2,
-    sabor: 'Banana com Nutella',
-    descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-    foto: 'assets/images/banana-com-nutella.png',
-    preco: 9.0,
-  },
-  {
-    id: 3,
-    sabor: 'Chocolate Belga',
-    descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-    foto: 'assets/images/chocolate-belga.png',
-    preco: 7.0,
-  },
-];
-
-app.get('/', async (req, res) => {
-  res.send('API Online');
-});
-
-app.get('/paletas/find-paletas', (req, res) => {
-  res.send(paletas);
-});
-
-app.get('/paletas/find-paleta/:id', (req, res) => {
-  const idPaleta = req.params.id;
-  const chosenPaleta = paletas.find((paleta) => paleta.id == idPaleta);
-  res.send(chosenPaleta);
-});
+app.use('/paletas', route);
 
 app.listen(port, () => {
   console.log(`A magica acontece em http://localhost:${port}`);
