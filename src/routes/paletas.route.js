d1106a1ffe79e9df3c5fe236aba1e8df9da31613
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-04-27 20:58:26 -0300

    criando swagger

--- src/routes/paletas.route.js
+++ src/routes/paletas.route.js
@@ -1,9 +1,14 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
 const route = require('express').Router();
 const controllerPaletas = require('../controllers/paletas.controller');
 const {
   validId,
   validObjectBody,
 } = require('../middlewares/paleta.middleware');
+
+route.use('/api-docs', swaggerUi.serve);
+route.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 route.get('/all-paletas', controllerPaletas.findPaletasController);
 route.get(
